0a22e46b698e188832575f9108337d56d35f03cf
Abhishek <ay143488@gmail.com>
2023-06-17 19:32:02 +0530

    Added styles for navbar ,footer, Home,Projects ans ProjectsDisplay

---
+++ personal-portfolio/src/helpers/ProjectList.js
@@ -0,0 +1,12 @@
+export const ProjectList = [
+    {
+      name: "TodoList",
+      image: Proj1,
+      skills: "JavaScript,HTML,CSS,NodeJS,MongoDB,EJS,",
+    },
+    {
+      name: "DayNews",
+      image: Proj2,
+      skills: "External API,NodeJs,JavaScript,HTML,CSS,EJS",
+    },
+  ];
